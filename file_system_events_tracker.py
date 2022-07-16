7763be76478c820212f7360cd6c58cf3f85f5d9b
AaravGup <82709271+AaravGup@users.noreply.github.com>
2022-07-16 12:26:24 +0530

    Add files via upload

---
+++ file_system_events_tracker.py
@@ -0,0 +1,31 @@
+import sys
+import time
+import random
+import os
+import shutil
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+from_dir = "C:/Users/nitgu/Downloads"
+
+class FileEventHandler(FileSystemEventHandler):
+    def on_created(self,event):
+        print(f"Hey, {event.src_path} has been created")
+    def on_deleted(self,event):
+        print(f"Oops! Someone deleted {event.src_path}")
+    def on_modified(self, event):
+        print(f"Hey, Someone modified {event.src_path}")
+    def on_moved(self, event):
+        print(f"Hey, Someone moved {event.src_path}")
+
+eventhandler = FileEventHandler()
+
+observer = Observer()
+observer.schedule(eventhandler,from_dir,recursive = True)
+observer.start()
+try:
+    while True:
+        time.sleep(2)
+        print("running...")
+except KeyboardInterrupt:
+    observer.stop()
